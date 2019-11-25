7e18032196ce72316d2d2154fee10867f6de920a
hritikaggarwal2 <hritikaggarwal2@gmail.com>
2019-11-24 18:45:40 -0800

    final workshop

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ console.log("hello");
 
 window.addEventListener("load", init)
 
+let BaseAPI = "https://api.openweathermap.org/data/2.5/weather?";
+let APIKey = "7b9ef6d5a3c36d00b45d1c53aa1413c9";
+
 function FtoC(degree) {
     let ret = degree - 32;
     ret = ret * 5 / 9;
@@ -22,7 +25,7 @@ function init() {
     buttonC.addEventListener("click", function(){
         let temp = document.querySelector("h1");
         let temp_num = parseInt(temp.innerText);
-        temp.innerText = FtoC(temp_num);
+        temp.innerText = FtoC(temp_num) + decodeURI("%C2%B0");
         buttonC.disabled = true;
         buttonF.disabled = false;
     })
@@ -30,18 +33,87 @@ function init() {
     buttonF.addEventListener("click", function(){
         let temp = document.querySelector("h1");
         let temp_num = parseInt(temp.innerText);
-        temp.innerText = CtoF(temp_num);
+        temp.innerText = CtoF(temp_num) + decodeURI("%C2%B0");
         buttonF.disabled = true;
         buttonC.disabled = false;
 
     })
 
-    changeImage("snowflake");
 
     function changeImage(new_img) {
         let image = document.getElementById("img");
         console.log(image);
-        image.src = "images/" + new_img + ".svg";
+        let imgs = ["cloudy", "rain", "semi_cloudy", "snowflake", "storm","sunny"];
+        
+        if (!imgs.includes(new_img.toLowerCase())) {
+            image.src = "images/cloudy.svg";
+        } else {
+            image.src = "images/" + new_img + ".svg";
+        }
+    }
+
+    changeLocation();
+    let buttonZip = document.getElementById("zip_btn");
+    let buttonCity = document.getElementById("city_btn");
+
+    buttonZip.addEventListener("click", function(){changeLocation("zip")});
+
+    buttonCity.addEventListener("click", function(){changeLocation("city")});
+
+    // new function created for updating the app!
+    function changeLocation(mode) {
+        // units system for having a proper temperature unit
+        let units = "metric";
+        if (buttonF.disabled) {
+            units = "imperial";
+        }
+
+        let key = "q=Seattle";
+        if(mode == "zip") {
+            let input = document.getElementById("get_zip");
+            key = "zip=" + input.value;
+        } else if (mode == "city") {
+            let input = document.getElementById("get_city")
+            key = "q=" + input.value;
+        }
+
+        console.log(key);
+
+        // main url from which you basically get the data
+        let url = BaseAPI + key + "&units=" + units + "&appid=" + APIKey;
+
+        // using the console to check what things to add
+        console.log(url);
+
+        // getting data from the url
+        fetch(url)
+            .then(function(data){
+                if (data.status >= 200 && data.status < 300) {
+                    return data.text();
+                } else {
+                    return 0;
+                }
+            })
+            .then(JSON.parse)
+            .then(function(data){
+                console.log(data);
+                let temp = document.querySelector("h1");
+                temp.innerText = Math.round(data.main.temp) + decodeURI("%C2%B0");
+
+                changeImage(data.weather[0].main);
+
+                let cast = document.querySelector("h4");
+                cast.innerText = data.weather[0].main;
+
+                let place = document.querySelector("h3");
+                place.innerText = data.name;
+
+                let country = document.querySelector("h5");
+                country.innerText = data.sys.country;
+            })
+            .catch(function(){
+                alert("Wrong Request!");
+            })
     }
 }
 
